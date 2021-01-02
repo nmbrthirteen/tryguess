50a1064fe6e7bd6679911b26d54c0009f93b010c
Nmbrthirteen <61732491+nmbrthirteen@users.noreply.github.com>
2021-01-02 16:11:59 +0400

    Add files via upload

--- tryguess.py
+++ tryguess.py
@@ -1,17 +1,26 @@
+import os
+import platform
+
+#Check Platform For Clear
+def platformCheck():
+    if platform.system() == "Windows":
+        os.system('cls')
+    elif platform.system() == "Linux":
+        os.system('clear')
 #Start
 def start():
+    platformCheck()
     print ("Hello dude! - Nmbrthirteen\n")
     print ("Your goal is to guess number, which i have in my mind \n")
     print ("also, you have only 10 attempts")
     print ("\nNumber is from 1 to 50")
-    try:
-        firstQuestion = input("Can we start?(yes or no): ")
-        if firstQuestion == "yes" or firstQuestion == "Yes":
-            trytoGuess();
-        else:
-            print("Okay, I can only say goodbye now :/")
-    except:
-        print("Okay, I can only say goodbye now ://")
+    firstQuestion = input("Can we start?(yes or no): ")
+    if firstQuestion == "yes" or firstQuestion == "Yes" or firstQuestion == "YES":
+        platformCheck()
+        trytoGuess();
+    else:
+        print("Okay, I can only say goodbye now :/")
+
 #Restart
 def start2():
     print ("\nHello again dude! - Nmbrthirteen")
@@ -45,16 +54,20 @@ def trytoGuess():
             print("\nYou have run out of attempts")
             lastQuestionisa = input("Do you need to play again?(yes or no): ")
             if lastQuestionisa == "yes" or lastQuestionisa == "Yes" or lastQuestionisa == "YES":
+                platformCheck()
                 start2();
             else:
                 print("\nOkay, Goodbye - Thanks for Playing!")
         #Correct Answer
         if toGuess == tryGuess:
-            print("Oh my god! you guessed it!!" or "Bravo! you guessed it!")
+            platformCheck()
+            print("Oh my god! you guessed it!! Correct Answer is {}".format(toGuess)  or "Bravo! you guessed it! Correct Answer is {}".format(toGuess))
             lastQuestion = input("Do you need to play again?(yes or no): ")
             if lastQuestion == "yes" or lastQuestion == "Yes" or lastQuestion == "YES":
+                platformCheck()
                 start2();
             else:
+                platformCheck()
                 print("\nOkay, Goodbye - Thanks for Playing!")
                 break
             
